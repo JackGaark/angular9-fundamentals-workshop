92e86c1112fbfb9e04fa01900d6f33461bbe89df
Vinpapounet <Vinpapounet@Jacks-MacBook-Air.local>
2020-06-08 13:39:08 -0400

    added routes

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,22 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  // CHALLENGE
+  // STEP 01: Display courses using ngFor
+  // STEP 02: Add event handler to select course
+  // STEP 03: Display raw json of selected course
+  selectedCourse = null;
+
   courses = [
     {
       id: 1,
       title: 'Angular 9 Fundamentals',
       description: 'Learn the fundamentals of Angular 9',
       percentComplete: 26,
-      favorite: true
-    }
+      favorite: true,
+    },
+    {
+      id: 2,
+      title: 'JavaScript The Really REALLY HARD PARTS',
+      description: 'Worship Will Sentance',
+      percentComplete: 50,
+      favorite: true,
+    },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectCourse(course) {
+    this.selectedCourse = course;
   }
 
+  deleteCourse(courseId) {
+    console.log('COURSE DELETED!', courseId);
+  }
 }
